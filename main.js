730de8724a0ceff8e132e4a7be504d3354d533d7
tsryu <yuteta@gmail.com>
2025-01-01 15:02:18 +0900

    add throttle desc

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 // 1. debounce
-// - reference - https://github.com/lodash/lodash/blob/main/dist/lodash.js#L10372
+// https://github.com/lodash/lodash/blob/8a26eb42adb303f4adc7ef56e300f14c5992aa68/dist/lodash.js#L10372
 // - debounce 함수 내부의 timerId는 debounce가 호출될 때 생성된 반환 함수의 렉시컬 스코프에 포함됨
 // - 반환된 함수는 timerId에 접근할 수 있는 상태로 유지되므로, 호출이 반복될 때마다 동일한 timerId 변수를 참조할 수 있음
 
+// 구현 팁 - timerId를 클로저에 저장했다가 clearTimeout하고 setTimeout으로 실행하는게 포인트
 const debounce = (func, delay) => {
   let timerId; // 타이머 ID를 저장할 변수
 
@@ -17,14 +18,16 @@ debounce(test, 1000);
 
 
 // 2. throttle
-const throttle = (func, interval) => {
-  let lastTime = 0;
-  
+// https://github.com/lodash/lodash/blob/8a26eb42adb303f4adc7ef56e300f14c5992aa68/dist/lodash.js#L10965
+
+// 구현 팁 - isWaiting과 setTimeout 후에 isWaiting 상태 바꾸기
+const throttle = (callback, wait) => {
+  let isWaiting = false;
   return (...args) => {
-    const now = Date.now();
-    if (now - lastTime >= interval) {
-      lastTime = now;
-      func(...args);
+    if (!isWaiting) {
+      callback(...args);
+      isWaiting = true;
+      setTimeout(() => (isWaiting = false), wait);
     }
   };
 };
