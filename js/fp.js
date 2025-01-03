7252a4e70a7c6ebcfa6224b8e8bd5db9c5068bc3
tsryu <yuteta@gmail.com>
2025-01-03 21:08:50 +0900

    chore: add go, pipe

--- js/fp.js
+++ js/fp.js
@@ -1,42 +1,73 @@
 // # Funtional Programming
 
+const log = console.log
+
 // ## 1. map
 // - 구현 포인트는 빈배열 만들어서 하나씩 밀어넣는 것
 const map = (fn, iter) => {
   const arr = [];
-  iter.forEach((item) => arr.push(fn(item)));
+  for (const a of iter) {
+    (a) => arr.push(fn(a))
+  }
   return arr;
 };
 
 const aa = map((a) => a * 2, [1, 2, 3, 4, 5]);
-console.log(aa);
+log(aa);
 
 // ## 2. filter
 const filter = (fn, iter) => {
   const arr = [];
-  iter.forEach((item) => {
-    if (fn(item)) arr.push(item);
-  });
+  for (const a of iter) {
+    if (fn(a)) arr.push(a);
+  };
   return arr;
 };
 
 const bb = filter((a) => a > 3, [1, 2, 3, 4, 5]);
-console.log(bb);
+log(bb);
 
 // ## 3. reduce
 // - 구현 포인트는 fn이 인자 2개라는 점, 기본값을 넘긴다는 부분임
-const reduce = (fn, initValue, iter) => {
-  let acc = initValue;
-  iter.forEach((item) => {
-    acc = fn(acc, item);
-  })
+// - iter가 없다면 acc를 받아서 처리하는 방식
+const reduce = (f, acc, iter) => {
+  if (!iter) {
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  }
+  for (const a of iter) {
+    acc = f(acc, a);
+  }
   return acc;
-}
-
+};
 const add = (a, b) => a + b;
 
-console.log(reduce(add, 0, [1, 2, 3, 4, 5]));
+log(reduce(add, 0, [1, 2, 3, 4, 5]));
 
-// ## 4. currying
+// ## 4. go
+// - 함수를 순서대로 적용해주는 함수
+const go = (...args) => reduce((a, f) => f(a), args);
 
-// ## 5. lazy evaluation
+log(
+  go(
+    add(0, 1),
+    (a) => a + 10,
+    (a) => a + 100
+  )
+);
+
+// ## 5. pipe
+// - go로 구성한 함수를 리턴하는 함수. 리턴한 함수를 사용해 시작부터 인자를 넘길 수 있다
+const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs);
+const pipedF = pipe(
+  (a, b) => a + b,
+  a => a + 10,
+  a => a + 100);
+
+// pipe는 f로 정의해서 인자를 이렇게 넘길 수 있다
+log(pipedF(0, 1)); // 111
+// ## 6. curry
+
+// ## 7. lazy evaluation + iterator
+
+// ## 8. Promise
